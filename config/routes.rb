bf048493bc5b6528aa394afcb2bdc96e60fc0ee6
takahashim <takahashim@tatsu-zine.com>
2014-12-01 18:02:11 +0000

    Postを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+
   get 'sessions/create'
 
   get 'sessions/destroy'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141201060834) do
+ActiveRecord::Schema.define(version: 20141201173328) do
+
+  create_table "posts", force: true do |t|
+    t.text     "comment"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "provider"
